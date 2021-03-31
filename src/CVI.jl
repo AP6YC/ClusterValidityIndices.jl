682946f92bfbec49628eb4f564fda935196e203d
Sasha Petrenko <sap625@mst.edu>
2021-03-31 14:59:59 -0500

    Port CVI code and tests

--- src/CVI.jl
+++ src/CVI.jl
@@ -1,5 +1,22 @@
 module CVI
 
-# Write your package code here.
+using Statistics: median, mean
+
+abstract type AbstractCVI end
+
+include("CVI/CVI.jl")
+
+# Export all public names
+export
+    # CVI
+    XB,
+    DB,
+    PS,
+    # CONN,
+    param_inc!,
+    param_batch!,
+    evaluate!,
+    get_icvi!,
+    get_cvi!
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using CVI
 using Test
 
+include("test_utils.jl")
+
 @testset "CVI.jl" begin
     # Write your tests here.
+    include("test_cvis.jl")
 end
