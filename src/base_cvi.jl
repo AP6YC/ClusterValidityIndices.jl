da982851ed4ca04cb3f1d70b29a7767c2bd7e72d
Sasha Petrenko <sap625@umsystem.edu>
2023-01-17 11:48:25 -0600

    Remove excl from non-mutating, add n and v updates

--- src/base_cvi.jl
+++ src/base_cvi.jl
@@ -138,8 +138,8 @@ function get_cvi_strategy(config::AbstractDict)
     strategy = CVIStrategy()
     for (name, subconfig) in config["params"]
         strategy[name] = CVIParamConfig(
-            Symbol(name * "_update!"),
-            Symbol(name * "_add!"),
+            Symbol(name * "_update"),
+            Symbol(name * "_add"),
             config["container"][subconfig["shape"]]["expand"],
             config["container"][subconfig["shape"]]["type"]{subconfig["type"]},
             subconfig["shape"],
@@ -213,35 +213,43 @@ function extend_strategy!(cvi::CVI, name::AbstractString, value::Any)
     eval(CVI_STRATEGY[name].expand)(cvi.params[name], value)
 end
 
-function add_strategy!(cvi::CVI, sample::RealVector, name::AbstractString)
+function add_strategy(cvi::CVI, sample::RealVector, name::AbstractString)
     return eval(CVI_STRATEGY[name].add)(cvi, sample)
 end
 
 function base_add_cluster!(cvi::CVI, sample::RealVector)
     for name in keys(cvi.params)
         # eval(CVI_STRATEGY[name].add)(cvi, sample)
-        cvi.cache[name] = add_strategy!(cvi, sample, name)
+        cvi.cache[name] = add_strategy(cvi, sample, name)
         extend_strategy!(cvi, name, cvi.cache[name])
     end
 end
 
-function n_add!(_::CVI, _::RealVector)
+function n_add(_::CVI, _::RealVector)
     return 1
 end
 
-function v_add!(_::CVI, sample::RealVector)
+function v_add(_::CVI, sample::RealVector)
     return sample
 end
 
-function CP_add!(cvi::CVI, sample::RealVector)
+function CP_add(cvi::CVI, sample::RealVector)
     return cvi.opts.CP_alt ? dot(sample, sample) : 0.0
 end
 
-function G_add!(cvi::CVI, sample::RealVector)
+function G_add(cvi::CVI, sample::RealVector)
     return cvi.opts.CP_alt ? sample : zeros(cvi.base.dim)
 end
 
-function CP_update(cvi::CVI, i_label::Integer)
+function n_update(cvi::CVI, ::RealVector, i_label::Integer)
+    return cvi.params["n"][i_label] + 1
+end
+
+function v_update(cvi::CVI, sample::RealVector, i_label::Integer)
+    return update_mean(cvi.params.["v"][:, i_label], sample, n_new)
+end
+
+function CP_update(cvi::CVI, ::RealVector, i_label::Integer)
     CP_new = (
         cvi.params["CP"][i_label]
         + dot(cvi.cache["diff_x_v"], cvi.cache["diff_x_v"])
@@ -251,7 +259,7 @@ function CP_update(cvi::CVI, i_label::Integer)
     return CP_new
 end
 
-function G_update(cvi::CVI, i_label::Integer)
+function G_update(cvi::CVI, ::RealVector, i_label::Integer)
     G_new = (
         cvi.params["G"][:, i_label]
         + cvi.cache["diff_x_v"]
@@ -268,9 +276,9 @@ function update_cluster!(cvi::CVI, sample::RealVector, i_label::Integer)
     # Compute delta_v and diff_x_v
     compute_cache!(cvi, sample, i_label)
     # Compute the CP_new
-    CP_new = CP_update(cvi, i_label)
+    CP_new = CP_update(cvi, sample, i_label)
     # Compute the G_new
-    G_new = G_update(cvi, i_label)
+    G_new = G_update(cvi, sample, i_label)
     # Update parameters
     update_params!(cvi.params, i_label, n_new, CP_new, v_new, G_new)
 end
